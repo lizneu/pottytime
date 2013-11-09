6efc3d138ac1da15ab9e487b40d08a6fdadafc72
Julie Mond <juliemond4@gmail.com>
2013-11-09 11:31:31 -0500

    Fixed detailed
    Not complete but not breaking

--- sockets/detailed.js
+++ sockets/detailed.js
@@ -1,9 +1,9 @@
 exports.onConnect = function(db){
-	var collection = db.get('potties');
+	var collection = db.collection('potties');
 	//Returns a function to handle a connection to a socket
 	return function(socket) {
 		socket.on("detailed", function(data) {
-			collection.findOne({_id: data.id}, {}, function(err, doc) {
+			collection.findOne({_id: data.id}).toArray(function(e,docs){
 				console.log(doc);
 				socket.emit("potty", doc);
 			});
